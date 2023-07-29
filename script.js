346fd0390bfeb0f693383c1198baad90f582205a
cc201007 <cc201007@fhstp.ac.at>
2023-07-29 10:18:05 +0200

    youtube fix

--- script.js
+++ script.js
@@ -2,19 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelector(".nav-links");
     const burger = document.querySelector(".burger");
   
-    // Function to toggle the mobile menu
     function toggleMenu() {
       navLinks.classList.toggle("nav-active");
       burger.classList.toggle("toggle");
     }
   
-    // Event listener for the burger menu button click
     burger.addEventListener("click", toggleMenu);
   
-    // Event listeners for menu item clicks to close the menu
     const navItems = document.querySelectorAll(".nav-links li");
     navItems.forEach((item) => {
       item.addEventListener("click", toggleMenu);
     });
   });
-  
